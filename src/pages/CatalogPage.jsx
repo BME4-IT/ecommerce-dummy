5ac097d0e05ef027d6f39b767b8131aa04ec44e9
Gora Indiraja Shafa <gora.indiraja@gmail.com>
2024-05-01 23:33:36 +0700

    setup catalog page

---
+++ src/pages/CatalogPage.jsx
@@ -0,0 +1,8 @@
+
+export default function CatalogPage() {
+    return(
+        <div>
+            <h1>Catalog Page</h1>
+        </div>
+    )
+}
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import RootLayout from "../layouts/RootLayout";
+import CatalogPage from "../pages/CatalogPage";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
                 path: "/",
                 element: <HomePage/>
             },
+            {
+                path: "/catalog",
+                element: <CatalogPage/>
+            }
         ]
     }
 ])
